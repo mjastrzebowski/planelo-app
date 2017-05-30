627c155471168771aa8788d36ab717b631f802c0
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2017-05-30 15:42:40 +0200

    TDD setup

--- src/config.prod.ts
+++ src/config.prod.ts
@@ -8,6 +8,10 @@ export const Config = {
   }
 };
 
+export const environment: any = {
+  production: true,
+};
+
 export const FIREBASE_URL: string = 'https://fiery-heat-1991.firebaseioo.com';
 export const FIREBASE_CLIENTS_URL: string = `${FIREBASE_URL}/cal_clients`;
 export const FIREBASE_PLACES_URL: string = `${FIREBASE_URL}/cal_places`;
--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,15 @@ export const Config = {
   }
 };
 
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+export const environment: any = {
+  production: false,
+};
+
 export const FIREBASE_URL: string = 'https://fiery-heat-1991.firebaseioo.com';
 export const FIREBASE_CLIENTS_URL: string = `${FIREBASE_URL}/cal_clients`;
 export const FIREBASE_PLACES_URL: string = `${FIREBASE_URL}/cal_places`;
