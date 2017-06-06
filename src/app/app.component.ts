556f61686af4964a631f3642cc48bfe2ce6854c3
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2017-06-06 14:50:37 +0200

    Basic i18n setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 import { Nav, MenuController } from 'ionic-angular';
 
@@ -57,10 +58,16 @@ export class PlaneloApp {
   showUserBar: boolean;
 
   constructor(
+    private translate: TranslateService,
     private utils: Utils,
     public menu: MenuController,
     public auth: AuthService
-  ) {}
+  ) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+  }
 
   ngOnInit(): void {
     this.utils.showLoading('Uruchamianie aplikacji...');
