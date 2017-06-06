d115639c90e91f4fbf4d5081729726c0b71e0113
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2017-06-06 16:22:47 +0200

    Fixed employee-detail-hours import

--- src/pages/employee/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './employee-detail-hours';
-export * from './employee-detail-vacations';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
-export * from './employee';
+export * from './employee/employee-detail-hours/employee-detail-hours';
+export * from './employee/employee-detail-vacations/employee-detail-vacations';
