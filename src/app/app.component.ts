b6b134dc4658054af26cb16ba9ad2765c99c611d
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2017-06-06 14:55:59 +0200

    Login page translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,10 +63,8 @@ export class PlaneloApp {
     public menu: MenuController,
     public auth: AuthService
   ) {
-    // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
-     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use('pl');
   }
 
   ngOnInit(): void {
